31e4c747da2e92b0ea0fcc2e2d3d3bd1c3de6921
zxh <hello@qq.com>
2026-04-29 15:54:58 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
-import 'package:shadcn_flutter/shadcn_flutter.dart';
+import 'package:shadcn_flutter/shadcn_flutter.dart' hide MenuItem;
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:tray_manager/tray_manager.dart';
+import 'package:window_manager/window_manager.dart';
 import 'app_usage/app_usage_service.dart';
 import 'clipboard/clipboard_service.dart';
 import 'db/database_helper.dart';
@@ -16,9 +18,18 @@ void main() async {
   try {
     WidgetsFlutterBinding.ensureInitialized();
     logInfo('WidgetsFlutterBinding initialized');
+
     sqfliteFfiInit();
     databaseFactory = databaseFactoryFfi;
     logInfo('sqflite initialized');
+
+    await windowManager.ensureInitialized();
+    windowManager.waitUntilReadyToShow().then((_) async {
+      await windowManager.setTitle('Super Tool');
+      await windowManager.setPreventClose(true);
+      await windowManager.show();
+    });
+
     networkService.start();
     sleep(.new(seconds: 1));
     logInfo('networkService started');
@@ -38,6 +49,18 @@ void main() async {
       logInfo('keyListenerService started');
     }
 
+    await trayManager.setIcon(
+      Platform.isWindows
+          ? 'assets/app_icon.ico'
+          : 'assets/app_icon.png',
+    );
+    await trayManager.setToolTip('Super Tool');
+    await trayManager.setContextMenu(Menu(items: [
+      MenuItem(key: 'show_window', label: '显示窗口'),
+      MenuItem.separator(),
+      MenuItem(key: 'exit_app', label: '退出程序'),
+    ]));
+
     logInfo('calling runApp');
     runApp(const MyApp());
   } catch (e, st) {
@@ -46,8 +69,53 @@ void main() async {
   }
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WindowListener, TrayListener {
+  @override
+  void initState() {
+    windowManager.addListener(this);
+    trayManager.addListener(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    trayManager.removeListener(this);
+    super.dispose();
+  }
+
+  @override
+  void onWindowClose() async {
+    await windowManager.hide();
+  }
+
+  @override
+  void onTrayIconMouseDown() {
+    windowManager.show();
+  }
+
+  @override
+  void onTrayIconRightMouseDown() {
+    trayManager.popUpContextMenu();
+  }
+
+  @override
+  void onTrayMenuItemClick(MenuItem menuItem) async {
+    switch (menuItem.key) {
+      case 'show_window':
+        await windowManager.show();
+        await windowManager.focus();
+      case 'exit_app':
+        await windowManager.destroy();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
